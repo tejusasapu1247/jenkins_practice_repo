55cb5eaae07227e39f63e439a5e44037a376be74
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2021-03-31 12:54:15 +0530

    Update HeloWorld.java

--- HeloWorld.java
+++ HeloWorld.java
@@ -1,8 +1,14 @@
 
 public class HeloWorld {
 	public static void main(String [] args) {
-		for(int i=0;i<5;i++){
+		
+// 		for(int i=0;i<5;i++){
+// 		System.out.println("hello world....!");
+// 		}
+		int i=5;
+		while(i){
 		System.out.println("hello world....!");
+			i--;
 		}
 		
 	}
